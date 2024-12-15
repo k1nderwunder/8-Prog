18cce88cbe6a08c8377ef9d32ecf58d944431351
k1nderwunder <maxday202020@gmail.com>
2024-12-16 03:13:38 +0700

    Version 1
    Обьявлены все классы, а методы в виде заглушек

---
+++ 8-Prog.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+class PalindromeFinder:
+    def __init__(self):
+        self.text = ""
+        self.palindromes = []
+        self.algorithm_executed = False
+
+    def main(self):
+        pass
+
+    def input_data(self):
+        pass
+
+    def generate_random_text(self):
+        pass
+
+    def find_palindromes(self, text):
+        pass
+
+    def print_palindromes(self, palindromes):
+        pass
+
+    def exit_program(self):
+        pass
+
+if __name__ == "__main__":
+    finder = PalindromeFinder()
+    finder.main()
